ffd13daa271781f0e347b5c26ee3f85359ac0af5
beverly <beverly.lopes@gmail.com>
2023-10-22 16:02:53 -0300

    meu primeiro projeto java

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Boas Vindas a toda a comunidade DIO");
+        System.out.println("Boas-vindo a todos da comunidade DIO");
     }
 }
